c35820705192c81d31a65b8a5711f5cfba906fdf
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-23 14:38:21 -0600

    first commit

---
+++ sound.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+
+playsound('bensound-ukulele.mp3')
+
+'''
+Would to work in MacOS except after installling
+"PyObjC" via `pip` per https://github.com/TaylorSMarks/playsound/issues/51
+'''
