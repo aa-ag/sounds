0494228cb45f9576e7aa4c053c7d10514877e947
aa-ag <aaron.aguerrevere@gmail.com>
2020-11-23 15:10:15 -0600

    converting to .wav

--- sound.py
+++ sound.py
@@ -1,8 +1,17 @@
 from playsound import playsound
+from pydub import AudioSegment
 
-playsound('bensound-ukulele.mp3')
+src = 'bensound-ukulele.mp3'
+
+# playsound(src)
 
 '''
 Would to work in MacOS except after installling
 "PyObjC" via `pip` per https://github.com/TaylorSMarks/playsound/issues/51
 '''
+
+# converting MP3 into .wav
+
+sound = AudioSegment.from_mp3(src)
+
+sound.export('bensound.wav', format='wav')
